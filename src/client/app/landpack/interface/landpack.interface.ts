6349e8ecf329eea64167bb17d9f0be9d3aa5337d
TaeHoon Kim <TaeHoon Kim>
2016-08-08 16:32:38 +0900

    파일생성

---
+++ src/client/app/landpack/interface/landpack.interface.ts
@@ -0,0 +1,15 @@
+export interface LandpackSearchResult {
+
+}
+
+export interface LandpackSearchResultDetail {
+
+}
+
+export interface LandpackBooking {
+
+}
+
+export interface LandpackBookingDetail {
+  
+}
